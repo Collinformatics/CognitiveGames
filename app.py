11c3296d40347cd8a499ed66e710e17d2781b0d6
Collin Andrews <179949844+Collinformatics@users.noreply.github.com>
2026-09-12 20:11:16 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -5,16 +5,17 @@ from memory import Memory
 
 
 parser = argparse.ArgumentParser(description='Cognitive Games')
-parser.add_argument('-m', '--maxvalue', type=int, default=15,
-                    help='Max value for the operation tables')
-parser.add_argument('-r', '--randomize', action='store_false',
-                       help='Decrease difficulty by ordering tables')
-parser.add_argument('-t', '--timer', type=float, default=1.0,
-                    help='Amount of time in seconds a digit is displayed in a memory game')
 parser.add_argument('--math', action='store_true',
-                       help='Start with the mathematics game')
+                    help='Begin with the mathematics games')
+parser.add_argument('-m', '--maxvalue', type=int, default=15,
+                    help='Max value for the operation tables (default=15)')
+parser.add_argument('-r', '--randomize', action='store_false',
+                    help='Decrease difficulty by ordering tables')
 parser.add_argument('--memory', action='store_true',
-                       help='Start with the memory game')
+                    help='Begin with the memory games')
+parser.add_argument('-t', '--timer', type=float, default=1.0,
+                    help='Amount of time in seconds a digit is displayed '
+                         'in a memory game (default=1 sec)')
 args = parser.parse_args()
 
 
@@ -22,17 +23,18 @@ args = parser.parse_args()
 mathGames = Math(maxValue=args.maxvalue, randomize=args.randomize)
 memoryGames = Memory(timer=args.timer)
 
+# Options
+games = {
+    '1': 'Mathematics',
+    '2': 'Memory',
+    'q': 'Quit'
+}
+
+# Startup options
 if args.math:
     mathGames.run()
 if args.memory:
     memoryGames.run()
-
-
-games = {
-        '1': 'Mathematics',
-        '2': 'Memory',
-        'q': 'Quit'
-    }
 
 
 if __name__ == '__main__':
